c761261ed9be3d674a2f334791d39f07b136768d
StarbzYT <chhokaradarsh@gmail.com>
2023-10-22 13:41:25 -0700

    resolved merge conflicts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,15 @@
-import React from "react"
-import Navbar from "../components/Navbar.jsx"
-import GoogleMaps from "../components/GoogleMaps.js"
 import React from 'react';
-import { Typography } from '@mui/material';
 import Navbar from '../components/Navbar.jsx';
+import GoogleMaps from '../components/GoogleMaps.js';
+import { Typography } from '@mui/material';
 
 function Home() {
   return (
     <div>
-      <Navbar logout="Logout" registerBusiness="Register Business"/>
+      <Navbar logout="Logout" registerBusiness="Register Business" />
       <Typography variant="h1">Home page</Typography>
       <GoogleMaps />
     </div>
-  )
+  );
 }
 export default Home;
