0642b14205bf029663c235516a77a358ea833246
Adam <afb2@sfu.ca>
2023-10-24 09:09:27 -0700

    privateRouteHome

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; // Import useState from 'react'
+import React, { useState } from 'react';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { Button } from '@mui/material';
 import app from '../api/firebase';
@@ -6,25 +6,27 @@ import { useNavigate } from 'react-router-dom';
 
 export default function LoginForm() {
   const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const GoogleSignIn = () => {
+  const GoogleSignIn = async () => {
     const provider = new GoogleAuthProvider();
     const auth = getAuth(app);
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const user = result.user;
-        if (user !== null) {
-          console.log(user);
-          navigate('/home');
-        } else {
-          navigate('/login');
-        }
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // Handle errors here
-      });
+
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      if (user !== null) {
+        console.log(user);
+        setIsAuthenticated(true);
+        navigate('/home', { state: { isAuthenticated: true } });
+      } else {
+        navigate('/login');
+      }
+    } catch (error) {
+      console.error('Google Sign-In Error:', error);
+      // Handle errors here
+    }
   }
 
   return (
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar.jsx';
 import GoogleMaps from '../components/GoogleMaps.js';
 import { Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isAuthenticated = location.state?.isAuthenticated;
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      // Redirect to the login page if the user is not authenticated
+      navigate('/login');
+    }
+  }, [isAuthenticated, navigate]);
+
   return (
     <div>
       <Navbar logout="Logout" registerBusiness="Register Business" />
@@ -12,4 +25,5 @@ function Home() {
     </div>
   );
 }
+
 export default Home;
