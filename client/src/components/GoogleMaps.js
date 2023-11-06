d64ff4ce4b4257d394212ab3f30cba0982507ccc
Yecheng Wang <64767165+Vancarii@users.noreply.github.com>
2023-11-05 23:18:16 -0800

    code cleanup

--- client/src/components/GoogleMaps.js
+++ client/src/components/GoogleMaps.js
@@ -9,18 +9,28 @@ import '../utils/Map.css';
 import '../api/helper.js';
 import { getAllBusinesses } from '../api/helper.js';
 
+/**
+ * Renders a Google Maps component with markers for businesses
+ * @returns {JSX.Element} Google Maps component
+ */
 const GoogleMaps = () => {
+  // Load Google Maps API
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
 
-  const [mapRef, setMapRef] = useState(null);
-  const [isOpen, setIsOpen] = useState(false);
-  const [center, setCenter] = useState({ lat: 0, lng: 0 });
-  const [infoWindowData, setInfoWindowData] = useState([]);
-  const [beacon, setBeacon] = useState(null);
-  const [markers, setMarkers] = useState([]);
+  // State variables
+  const [mapRef, setMapRef] = useState(null); // Reference to the Google Maps instance
+  const [isOpen, setIsOpen] = useState(false); // Whether an info window is open
+  const [center, setCenter] = useState({ lat: 0, lng: 0 }); // Center of the map
+  const [infoWindowData, setInfoWindowData] = useState([]); // Data for the info window
+  const [beacon, setBeacon] = useState(null); // Marker for the user's location
+  const [markers, setMarkers] = useState([]); // Markers for the businesses
 
+  /**
+   * Callback function when the map is loaded
+   * @param {Object} map - Google Maps instance
+   */
   const onMapLoad = (map) => {
     if (isLoaded) {
       setMapRef(map);
@@ -34,6 +44,13 @@ const GoogleMaps = () => {
     }
   };
 
+  /**
+   * Callback function when a marker is clicked
+   * @param {number} id - Index of the marker
+   * @param {number} lat - Latitude of the marker
+   * @param {number} lng - Longitude of the marker
+   * @param {string} address - Address of the marker
+   */
   const handleMarkerClick = (id, lat, lng, address) => {
     mapRef?.panTo({ lat, lng });
     setInfoWindowData({ id, address });
@@ -41,6 +58,7 @@ const GoogleMaps = () => {
     mapRef?.setZoom(13);
   };
 
+  // Get the user's location and set the center of the map
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -48,6 +66,7 @@ const GoogleMaps = () => {
         setCenter({ lat: latitude, lng: longitude });
         mapRef?.panTo({ lat: latitude, lng: longitude });
 
+        // Add a marker for the user's location
         if (isLoaded) {
           setBeacon(
             new window.google.maps.Marker({
@@ -68,6 +87,7 @@ const GoogleMaps = () => {
     }
   }, [mapRef]);
 
+  // Get the locations of all businesses and add markers for them
   useEffect(() => {
     getAllBusinesses().then((allBusinesses) => {
       const promises = allBusinesses.map((business) => {
@@ -96,6 +116,7 @@ const GoogleMaps = () => {
     });
   }, []);
 
+  // Render the Google Maps component
   if (loadError) {
     return <div>Error loading Google Maps</div>;
   }
