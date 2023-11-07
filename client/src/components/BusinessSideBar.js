5253c08db78562916008df45691f40e0a5bf483a
Yecheng Wang <64767165+Vancarii@users.noreply.github.com>
2023-11-06 23:36:09 -0800

    fixed places api business info retrieval

--- client/src/components/BusinessSideBar.js
+++ client/src/components/BusinessSideBar.js
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import StarIcon from "@mui/icons-material/Star";
+import { yellow } from "@mui/material/colors";
 
 /**
  * Renders a sidebar component for a business, displaying its location, name, promotion, and promotion period.
@@ -11,47 +13,91 @@ import React, { useEffect, useState } from 'react';
  * @param {Object} props.map - The Google Maps object.
  * @returns {JSX.Element} - The JSX element representing the sidebar component.
  */
-function BusinessSideBar({location, name, promotion, promotionPeriod, map}) {
+function BusinessSideBar({ location, name, promotion, promotionPeriod, map }) {
+  const [places, setPlaces] = useState([]); // Stores places returned from Google Maps API
 
-    const [places, setPlaces] = useState([]); // Stores places returned from Google Maps API
+  useEffect(() => {
+    if (!map) return; // If map or google is not loaded yet, don't try to run the following code
 
-    useEffect(() => {
-        if (!map) return; // If map or google is not loaded yet, don't try to run the following code
+    const service = new window.google.maps.places.PlacesService(map);
+    // service.google.maps.places.LocationBias = "IP_BIAS";
 
-        const service = new window.google.maps.places.PlacesService(map);
+    const request = {
+      query: String(name) + " " + String(location),
+      fields: ["name", "geometry", "formatted_address", "photos", "rating"],
+    };
 
-        const request = {
-        query: String(location),
-        fields: ['name', 'geometry', 'formatted_address'],
-        };
+    service.findPlaceFromQuery(request, (results, status) => {
+      if (
+        status === window.google.maps.places.PlacesServiceStatus.OK &&
+        results
+      ) {
+        setPlaces(results[0]);
+        // console.log(results[0]);
+      }
+    });
+  }, [location, name, promotion, promotionPeriod, map]); // Re-run this effect whenever map or location changes
 
-        service.findPlaceFromQuery(request, (results, status) => {
-        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-            setPlaces(results);
-        }
-        });
-    }, [location, name, promotion, promotionPeriod, map]); // Re-run this effect whenever map or location changes
-
-
-    return (
-        
-        !location
-        ? <div className="side-bar-empty" style={{position: 'absolute', zIndex: 2, padding: '20px'}}>
-            Select a business to view more information
-        </div> 
-        : <div className="side-bar" style={{position: 'absolute', zIndex: 2, padding: '20px'}}>
-            {places.map((place, index) => (
-            <div key={index}>
-                <h2 style={{margin: '20px 0'}}>{name.toUpperCase()}</h2>
-                <p style={{margin: '20px 0'}}>{place.formatted_address}</p>
-                <p style={{marginTop: '20px 0', marginBottom: '5px'}}>Current Promotion:</p>
-                <h2 style={{marginBottom: '20px 0'}}>{promotion}</h2>
-                <p style={{margin: '20px 0'}}>Promotion ends in: {promotionPeriod} hours</p>
-            </div>
-            ))}
-        </div>
-    )
+  return !location ? (
+    <div
+      className="side-bar-empty"
+      style={{ position: "absolute", zIndex: 2, padding: "20px" }}
+    >
+      Select a business to view more information
+    </div>
+  ) : (
+    <div
+      className="side-bar"
+      style={{ position: "absolute", zIndex: 2, padding: "20px" }}
+    >
+      {/* {places.map((place, index) => ( */}
+      {/* <div key={index}> */}
+      <div>
+        {places?.photos && (
+          <img
+            src={places?.photos[0].getUrl()}
+            alt="Business"
+            style={{ width: "70%", height: "auto", marginBottom: "10px" }}
+          />
+        )}
+        <h2>
+          <span style={{ marginBottom: "20px 0", color: "gold" }}>
+            {places?.rating}
+          </span>
+          <StarIcon sx={{ color: yellow[500] }} />
+        </h2>
+        <h2 style={{ margin: "20px 0" }}>{places?.name.toUpperCase()}</h2>
+        <p style={{ margin: "20px 0" }}>{places?.formatted_address}</p>
+        <p style={{ marginTop: "20px 0", marginBottom: "5px" }}>
+          Current Promotion:
+        </p>
+        <h2 style={{ marginBottom: "20px 0" }}>{promotion}</h2>
+        <p style={{ margin: "20px 0" }}>
+          Promotion ends in: {promotionPeriod} hours
+        </p>
+        <Button
+          variant="contained"
+          href={`https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(
+            places?.name
+          )}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Directions
+        </Button>
+        {/* <a
+          href={`https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(
+            places?.name
+          )}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Directions
+        </a> */}
+      </div>
+      {/* ))} */}
+    </div>
+  );
 }
 
 export default BusinessSideBar;
-
